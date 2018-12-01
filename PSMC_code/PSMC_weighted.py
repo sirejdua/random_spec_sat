7672d36f0d094087679893cf0dee7d798d8aaead
LAKSHYA JAIN <Lakshya.jain@berkeley.edu>
2018-11-30 22:38:46 -0800

    added multiplicative weights

--- PSMC_code/PSMC_weighted.py
+++ PSMC_code/PSMC_weighted.py
@@ -1,7 +1,8 @@
 import subprocess, os, sys
 import time
 import argparse
-from partition_random_sample import *
+# from partition_random_sample_clause_heuristic import *
+from partition_random_sample_mw import *
 import math
 ##########################################################
 ### PSMC on a CNF file without Aiger circuit unrolling ###
@@ -16,14 +17,14 @@ parser.add_argument("filename", help = "cnf file path")
 parser.add_argument("-k", "--num_partition_variables", help = "number of partitioning variables", type = int, default = -1)
 parser.add_argument("-eo", "--epsilon_original", help = "epsilon bound on original", type = float, default = 2)
 parser.add_argument("-ep", "--epsilon_partition", help = "epsilon value for partition at start", type = float, default = 2)
-parser.add_argument("-do", "--delta_original", help = "the delta for the original", type = float, default = .1)
+parser.add_argument("-do", "--delta_original", help = "the delta for the original", type = float, default = .05)
 parser.add_argument("-dp", "--delta_partition", help = "the delta for the partitions", type = float, default = .99)
 parser.add_argument("-ap", "--actual_probability", help = "don't calculate the exact probability; just use this number", type = float, default = -1)
 parser.add_argument("-epul", "--epsilon_partition_upper_limit", help = "upper limit on epsilon used on any larger cell sizes", type = float, default = 5)
-parser.add_argument("-tl", "--threshold_lower_limit", help = "the lower bound on the threshold to be used", type = float, default = 16)
+parser.add_argument("-tl", "--threshold_lower_limit", help = "the lower bound on the threshold to be used", type = float, default = 32)
 parser.add_argument("--method", action='store', choices=["3n/4","n/2", "n-5", "nlogn"], default = "nlogn", help='partitioning technique')
 parser.add_argument("--threshold", help='number of iterations to convergence', type = float, default = 32)
-parser.add_argument("--convergence_limit", help='the bound to which the density must converge to', type = float, default = 0.001)
+parser.add_argument("--convergence_limit", help='the bound to which the density must converge to', type = float, default = 0.0005)
 parser.add_argument("--ignore_original", help = "run scalmc on the original model", action = "store_true")
 parser.add_argument("--ignore_partition", help = "run scalmc on the original model", action = "store_true")
 args = parser.parse_args()
@@ -114,7 +115,7 @@ if run_on_partition:
             k = n-5
     # print("Partitioning Technique: " + str(args.method))
     # print("k = " + str(k))
-    k = 3
+
     # PSMC decreases the threshold for the steps needed to convergence as we expand the cell size. what should be the lower limit?
     threshold_lower_limit = int(args.threshold_lower_limit)
     # What should be the limit of convergence?
@@ -123,6 +124,7 @@ if run_on_partition:
     threshold = int(args.threshold)
     # How many free variables
     free_vars = n - k
+    k = 5
     #partition the file, time it
     start = time.time()
     variable_order = get_top_vars(k, 5000, filename)
@@ -154,22 +156,18 @@ if run_on_partition:
     empty_counter = 0 # how many empty partitions have i sampled?
     delta = 0.1
     level = 1 # the number of values k has taken on.
-    i = 0
+    window_count = 0
     min_k = int(math.log(n, 2))
-    #find p
-    # p = 0
-    # lim = n
-    # while lim > 0:
-    #     lim = lim - (2**p) * math.log(n, 2)
-    #     p += 1
-    # p -= 1
     final_start_time = time.time()
+
+    #probability array for boosting
+    # probs = np.zeros(k)
+    num_partitions_sampled = 0
     while not converged:
 
         # What is k (# of partitioning variables) currently at?  It is the max of (min_k, decremented_k)
         # because the idea is to reduce k by decreasing by 2 * math.log(n, 2) until it reaches the minimum possible size
         # once you reach the minimum possible size, stop decreasing k.
-
         decremented_k = int(n - (2**(level-1))*math.log(n, 2))
         min_k = int(math.log(n, 2))
         # if we have sampled a sufficient # of empty partitions....
@@ -189,7 +187,7 @@ if run_on_partition:
             partitions_sampled = 0
             density = 0
             # reset_count()
-            i = 0
+            window_count = 0
             # p = max (p-1, 0)
             decremented_k = int(n - (2**(level-1))*math.log(n, 2))
             min_k = int(math.log(n, 2))
@@ -200,6 +198,7 @@ if run_on_partition:
                 k = max(k-10, 0)
             # print("k: " + str(k))
             partition_vars = variable_order[:k]
+            # probs = np.zeros(k)
             final_start_time = time.time()
 
         old_density = density
@@ -212,11 +211,16 @@ if run_on_partition:
             if len(partitions) == 2**k:
                 converged = True
                 break
+        if converged:
+            break
+        assignment_arr = np.array([int(var_assignment) for var_assignment in assignment_str])
+        # probs = update_weights(assignment_arr, probs)
         end_gen = time.time()
         partitions.add(assignment_str)
         # write the partition to file
-        write_partition(partition_vars, filename, i, bin_string = assignment_str)
-        info = os.popen("./../../maxcount/scalmc --pivotAC " + str(pivotAC_partition) + " --delta " + str(delta_partition) + " " + filename.split('.cnf')[0] + "-window-" + str(i) + ".cnf").readlines()[-1]
+        write_partition(partition_vars, filename, window_count, bin_string = assignment_str)
+        info = os.popen("./../../maxcount/scalmc --pivotAC " + str(pivotAC_partition) + " --delta " + str(delta_partition) + " " + filename.split('.cnf')[0] + "-window-" + str(window_count) + ".cnf").readlines()[-1]
+        num_partitions_sampled += 1
         # info = os.popen("./../../maxcount/scalmc --pivotAC " + str(pivotAC_partition) + " --delta " + str(delta) + " " + filename.split('.cnf')[0] + "-window-" + str(i) + ".cnf").readlines()[-1]
         # info = os.popen("./../../maxcount/scalmc " + filename.split('.cnf')[0] + "-window-" + str(i) + ".cnf").readlines()[-1]
         partitions_sampled += 1
@@ -256,7 +260,7 @@ if run_on_partition:
                 density_counter = 0
         # print(density)
         total_files += 1
-        i += 1
+        window_count += 1
     end = time.time()
     partition_time = end - start
     partition_count = density * 2**n
@@ -285,7 +289,9 @@ if not args.ignore_original:
 print(result_info)
 print("************************************************")
 if not args.ignore_partition:
-    print("Time for partitioned with partitioning overhead : " + '{:.{p}g}'.format(partition_time + file_gen_time, p=4))
+    print("Total Time Taken: " + '{:.{p}g}'.format(partition_time + file_gen_time, p=4))
+    print("Time taken for file generation: " + str(file_gen_time))
+    print("Time taken for algorithm: " + str(partition_time))
     print("Time taken for final k value: " + str(end - final_start_time))
 if not args.ignore_original:
     print("Time for original: " + '{:.{p}g}'.format(original_time, p=4))
@@ -296,7 +302,7 @@ if not args.ignore_original:
     if not args.ignore_partition:
         print("Percent Error: " + str(100*abs((density - float(original_count)/(2**n))/(float(original_count)/(2**n)))))
 print("************************************************")
-
+print(num_partitions_sampled)
     # partition_count_str = str(int(partition_count)/(2**i)) + " x 2^" + str(i)
     # print("************************************************")
     # print("Convergence Limit: " + str(convergence_limit))
---
+++ PSMC_code/partition_random_sample_mw.py
@@ -0,0 +1,215 @@
+from cnf_creator import *
+import sys
+import os
+import math
+import random
+import pycryptosat
+import time
+import itertools
+import argparse
+import subprocess
+import numpy as np
+
+###################################
+##### RANDOM SAMPLER ##############
+###################################
+
+randbool = lambda: random.random() < 0.5
+
+def random_string_generator(k):
+    assignment_str = ""
+    for i in range(k):
+        if randbool():
+            assignment_str += "1"
+        else:
+            assignment_str += "0"
+    return assignment_str
+
+def solverForSample(clauses):
+    # set up solver with clauses from original formula
+    solver = pycryptosat.Solver()
+    for clause in clauses:
+        solver.add_clause(clause)
+    return solver
+
+def sample_solutions(numSamples, counting_vars, clauses): 
+    solver = solverForSample(clauses)
+    # take samples
+    positiveSamples = 0
+    loss_vector = np.zeros(max(counting_vars.keys()) + 1)
+    ones_loss_vector = np.zeros(max(counting_vars.keys()) + 1)
+    zero_loss_vector = np.zeros(max(counting_vars.keys()) + 1)
+    ones_probs = np.zeros(max(counting_vars.keys()) + 1)
+    zero_probs = np.zeros(max(counting_vars.keys()) + 1)
+    for T in range(numSamples):
+        eta = 1/np.sqrt(T + 1)
+        # generate random assignment to the counting variables
+        assumptions = []
+        counting_vars_iteration_value = dict()
+        for var in counting_vars.keys():
+            if randbool() > zero_probs[var]:
+                assumptions.append(var)
+                counting_vars_iteration_value[var] = 1
+            else:
+                assumptions.append(-var)
+                counting_vars_iteration_value[var] = 0
+        # check if assignment is consistent
+        sat, model = solver.solve(assumptions)
+        eta = 0.1
+        outcome = int(sat)
+        for var in counting_vars_iteration_value.keys():
+            ones_loss_vector[var] += abs(outcome - 1)
+            zero_loss_vector[var] += abs(outcome) 
+        if sat:
+            positiveSamples += 1
+        assumptions = [abs(v) for v in assumptions]
+        for var_assignment in assumptions: 
+            one_weight = np.exp(-eta * ones_loss_vector[var_assignment])
+            zero_weight = np.exp(-eta * zero_loss_vector[var_assignment])
+            total = one_weight + zero_weight
+            ones_probs[var], zero_probs[var] = one_weight/total, zero_weight/total
+            # if total == 0:
+            #     print(ones_loss_vector[var_assignment])
+            #     print(zero_loss_vector[var_assignment])
+            #     print(T)
+            counting_vars[var_assignment] = max(ones_probs[var], zero_probs[var] )
+            # else:
+            #     print("am I parsing a zero?")
+    return positiveSamples
+def countSampleWithMonteCarlo(numMCSamples, counting_vars, clauses):
+    solver = solverForSample(clauses)
+
+    # if formula is UNSAT, don't bother sampling
+    sat, model = solver.solve()
+    if not sat:
+        return (0, 0, True)
+
+    # take samples
+    positiveSamples = 0
+    for i in range(numMCSamples):
+        # generate random assignment to the counting variables
+        assumptions = []
+        for var in counting_vars.keys():
+            if randbool():
+                assumptions.append(var)
+            else:
+                assumptions.append(-var)
+
+        # check if assignment is consistent
+        sat, model = solver.solve(assumptions)
+        if sat:
+            positiveSamples += 1
+
+    sampleMant = float(positiveSamples) / numMCSamples
+    sampleExp = len(counting_vars.keys())
+    while sampleMant > 0 and sampleMant < 1:
+        sampleMant *= 2
+        sampleExp -= 1
+    return (sampleMant, sampleExp, False)
+
+#############################
+##### PARTITIONER ###########
+#############################
+
+num_clauses_initial = 0
+output = ""
+counting_vars = dict()
+clauses = []
+allOne = 0
+def get_top_vars(k, numSamples, filename):
+    global num_clauses_initial
+    #finds top k partitioning variables of formula after c random samples drawn from the counting variables
+    var_counts = np.zeros(k)
+    global counting_vars
+    #get the number of variables and create the appropriate array
+    global clauses
+    #get the number of positive samples
+    global allOne
+    counter = 0
+    with open(filename, 'r') as f:
+        for f_line in f:
+            line = f_line.split(' ')
+            if "c ind" in f_line:
+                ind_vars = line[2:-1]
+                counter += 1
+                for i in range(len(ind_vars)):
+                    counting_vars[int(ind_vars[i])] = 0
+            elif line[0] == 'p':
+                num_vars_total = int(line[2])
+                var_counts = np.zeros(num_vars_total + 1)
+                #if no ind vars are specified
+            elif line[0] == 'c' and counter != 0:
+                continue
+            else:
+                clauses.append([int(i) for i in line[:-1]])
+                counter += 1
+    if len(counting_vars.keys()) == 0:
+        for i in range(1, len(var_counts)):
+            counting_vars[i] = 0
+    counter = sample_solutions(numSamples, counting_vars, clauses)
+    allOne = (counter == numSamples)
+    for i in counting_vars.keys():
+        var_counts[i] = counting_vars[i]
+    #get k top vars
+    # for i in range(len(var_counts)):
+    #     if i in counting_vars:
+    #         if var_counts[i] < counter - var_counts[i]:
+    #             var_counts[i] = counter - var_counts[i]
+    var_counts = np.argsort(-var_counts)
+    return var_counts
+
+def partition_formula(var_counts, filename): 
+    n = len(var_counts)
+    for index in range(2**n):
+        #get binary string corresponding to index
+        write_partition(var_counts, filename, index)
+
+def write_partition(var_counts, filename, index, bin_string = None):
+    #var counts: an array with index i representing the ith variable to partition on.
+    n = len(var_counts)
+    b = bin(index)[2:]
+    l = len(b)
+    b = str(0) * (n - l) + b
+    if bin_string is not None:
+        b = bin_string
+    ##construct constraint
+    assignment = ""
+    for i in range(len(b)):
+        if b[i] == '1':
+            assignment += str(var_counts[i]) + ' 0\n'
+        else:
+            assignment += '-' + str(var_counts[i]) + ' 0\n'
+    #add constraint to original file
+    with open(filename, 'r') as file:
+        data = file.readlines()
+    num_data = []
+    for i in range(len(data)):
+        if "p cnf" in data[i]:
+            num_data = data[i].split(' ')
+            num_data[-1] = str(int(num_data[-1]) + n) 
+            constraints_and_clauses = num_data[0] + ' ' + num_data[1] + ' ' + num_data[2] + ' ' + num_data[3] + '\n'
+            data[i] = constraints_and_clauses
+            break
+    ###write modified info back to the file
+    name = filename.split('.cnf')[0] + '-window-' + str(index) + '.cnf'
+    with open(name, 'w') as f:
+        f.writelines(data)
+        f.write(assignment)
+    f.close()
+
+#################################
+##### END PARTITIONER ###########
+#################################
+if __name__ == "__main__":
+    output = str(sys.argv[1])
+    try:
+        k = int(sys.argv[2])
+    except:
+        k = 4
+    # with open(output + ".cnf", 'r') as f:
+    #     data = f.readlines()
+    #     with open(output + "copy.cnf", 'w') as filecopy:
+    #         filecopy.writelines(data)
+    partition_formula(get_top_vars(k, 50000, output), output)
+
+
